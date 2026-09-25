d134fdecf0695b0517d9c422c6c2abf68989fa7b
AnantPandey-dev <pandeyanant1820@gmail.com>
2026-09-25 19:40:44 +0530

    Add files via upload
    Butterfly pattern i made in c++

---
+++ butterfly_pattern.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    //upper half
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        for(int j=2*(n-i); j>0; j--){
+            if(j==2){
+                break;
+            }
+            cout << " ";
+        }
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    //lower half
+    for(int i=n-1; i>0; i--){
+        for(int j=i; j>0; j--){
+            cout << "*";
+        }
+        for(int j=2*(n-i); j>0; j--){
+            cout << " ";
+        }
+        for(int j=i; j>0; j--){
+            cout << "*";
+        }cout << endl;
+    }
+}
